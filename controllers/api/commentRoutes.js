34df80a605ba3f1fac7f94561a105177f7741445
Ryan Gurley <gurleyryan@gmail.com>
2024-04-19 16:56:49 -0400

    edited api controllers

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Comment, User, Blog } = require("../../models");
+const { Comment } = require("../../models");
 const withAuth = require("../../utils/withAuth");
 
 router.post("/", withAuth, async (req, res) => {
